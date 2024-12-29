fa4692b76ca37de3744eaac78e549f6f2c89cb93
gentlementlegen <fernand.veyrier@epitech.eu>
2024-12-29 18:56:27 +0900

    fix: the user role is determined from the repo when unavailable from organization

--- src/handlers/shared/get-user-task-limit-and-role.ts
+++ src/handlers/shared/get-user-task-limit-and-role.ts
@@ -7,7 +7,7 @@ interface MatchingUserProps {
 
 export async function getUserRoleAndTaskLimit(context: Context, user: string): Promise<MatchingUserProps> {
   const orgLogin = context.payload.organization?.login;
-  const { config, logger } = context;
+  const { config, logger, octokit } = context;
   const { maxConcurrentTasks } = config;
 
   const minUserTaskLimit = Object.entries(maxConcurrentTasks).reduce((minTask, [role, limit]) => (limit < minTask.limit ? { role, limit } : minTask), {
@@ -21,15 +21,40 @@ export async function getUserRoleAndTaskLimit(context: Context, user: string): P
       throw new Error("Invalid organization name");
     }
 
-    const response = await context.octokit.rest.orgs.getMembershipForUser({
-      org: orgLogin,
+    let role = "contributor";
+    let limit;
+
+    try {
+      const response = await octokit.rest.orgs.getMembershipForUser({
+        org: orgLogin,
+        username: user,
+      });
+      role = response.data.role.toLowerCase();
+      limit = maxConcurrentTasks[role];
+    } catch (err) {
+      logger.error("Could not get user membership", { err });
+    }
+
+    // If we failed to get organization membership, narrow down to repo role
+    const permissionLevel = await octokit.rest.repos.getCollaboratorPermissionLevel({
       username: user,
+      owner: context.payload.repository.owner.login,
+      repo: context.payload.repository.name,
     });
+    role = permissionLevel.data.role_name?.toLowerCase();
+    context.logger.debug(`Retrieved collaborator permission level for ${user}.`, {
+      user,
+      owner: context.payload.repository.owner.login,
+      repo: context.payload.repository.name,
+      isAdmin: permissionLevel.data.user?.permissions?.admin,
+      role,
+      data: permissionLevel.data,
+    });
+    if (role && maxConcurrentTasks[role]) {
+      limit = maxConcurrentTasks[role];
+    }
 
-    const role = response.data.role.toLowerCase();
-    const limit = maxConcurrentTasks[role];
-
-    return limit ? { role, limit } : minUserTaskLimit;
+    return limit ? { role, limit } : { ...minUserTaskLimit, role };
   } catch (err) {
     logger.error("Could not get user role", { err });
     return minUserTaskLimit;
