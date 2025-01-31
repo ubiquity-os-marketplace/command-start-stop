03a9bdb694ac81a1202658fbdd56583655d1166d
gentlementlegen <fernand.veyrier@epitech.eu>
2025-01-31 18:04:47 +0900

    test: fixed pull-request test to strictly equal the raw output

--- tests/pull-request.test.ts
+++ tests/pull-request.test.ts
@@ -1,13 +1,11 @@
 import { afterAll, afterEach, beforeAll, beforeEach, describe, expect, jest } from "@jest/globals";
 import { drop } from "@mswjs/data";
 import dotenv from "dotenv";
-import { createAdapters } from "../src/adapters";
-import { start } from "../src/handlers/shared/start";
 import { Context } from "../src/types";
 import { db } from "./__mocks__/db";
 import issueTemplate from "./__mocks__/issue-template";
 import { server } from "./__mocks__/node";
-import { createContext, getSupabase } from "./main.test";
+import { createContext } from "./main.test";
 
 dotenv.config();
 
@@ -48,7 +46,7 @@ async function setupTests() {
   });
 }
 
-describe("Collaborator tests", () => {
+describe("Pull-request tests", () => {
   beforeEach(async () => {
     drop(db);
     jest.clearAllMocks();
@@ -57,36 +55,13 @@ describe("Collaborator tests", () => {
     await setupTests();
   });
 
-  it("Should return error if user trying to assign is not a collaborator", async () => {
-    db.users.create({
-      id: 3,
-      login: "user1",
-      role: "contributor",
-    });
+  it("Should properly update the close status of a linked pull-request", async () => {
     const issue = db.issue.findFirst({ where: { id: { equals: 1 } } }) as unknown as Issue;
-    const sender = db.users.findFirst({ where: { id: { equals: 3 } } }) as unknown as PayloadSender;
-    const context = createContext(issue, sender, "/start");
-    context.adapters = createAdapters(getSupabase(), context);
-    await expect(start(context, issue, sender, [])).rejects.toMatchObject({
-      logMessage: {
-        diff: expect.stringContaining("Only collaborators can be assigned to this issue"),
-        level: "error",
-        raw: "Only collaborators can be assigned to this issue.",
-        type: "error",
-      },
-    });
-  });
+    issue.labels = [];
+    const sender = db.users.findFirst({ where: { id: { equals: 1 } } }) as unknown as PayloadSender;
 
-  it("should assign the author of the pull-request and not the sender of the edit", async () => {
-    db.users.create({
-      id: 3,
-      login: "ubiquity-os-sender",
-      role: "admin",
-    });
-    const issue = db.issue.findFirst({ where: { id: { equals: 1 } } }) as unknown as Issue;
-    const sender = db.users.findFirst({ where: { id: { equals: 3 } } }) as unknown as PayloadSender;
-    const context = createContext(issue, sender, "") as Context<"pull_request.edited">;
-    context.eventName = "pull_request.edited";
+    const context = createContext(issue, sender, "") as Context<"pull_request.opened">;
+    context.eventName = "pull_request.opened";
     context.payload.pull_request = {
       html_url: "https://github.com/ubiquity-os-marketplace/command-start-stop",
       number: 1,
@@ -96,6 +71,11 @@ describe("Collaborator tests", () => {
       },
     } as unknown as Context<"pull_request.edited">["payload"]["pull_request"];
     context.octokit = {
+      rest: {
+        pulls: {
+          update: jest.fn(),
+        },
+      },
       graphql: {
         paginate: jest.fn(() =>
           Promise.resolve({
@@ -119,21 +99,34 @@ describe("Collaborator tests", () => {
         ),
       },
     } as unknown as Context<"pull_request.edited">["octokit"];
-
     jest.unstable_mockModule("@supabase/supabase-js", () => ({
       createClient: jest.fn(),
     }));
     jest.unstable_mockModule("../src/adapters", () => ({
       createAdapters: jest.fn(),
     }));
-    const start = jest.fn();
-    jest.unstable_mockModule("../src/handlers/shared/start", () => ({
-      start,
-    }));
     const { startStopTask } = await import("../src/plugin");
-    await startStopTask(context);
-    // Make sure the author is the one who starts and not the sender who modified the comment
-    expect(start).toHaveBeenCalledWith(expect.anything(), expect.anything(), { id: 1, login: userLogin }, []);
-    start.mockClear();
+    await expect(startStopTask(context)).rejects.toMatchObject({
+      logMessage: {
+        raw: expect.stringContaining("No price label is set to calculate the duration"),
+      },
+    });
+    context.octokit = {
+      ...context.octokit,
+      //@ts-expect-error partial mock of the endpoint
+      paginate: jest.fn(() => []),
+      rest: {
+        ...context.octokit.rest,
+        orgs: {
+          //@ts-expect-error partial mock of the endpoint
+          getMembershipForUser: jest.fn(() => ({ data: { role: "member" } })),
+        },
+      },
+    };
+    await expect(startStopTask(context)).rejects.toMatchObject({
+      logMessage: {
+        raw: "Error: This task does not reflect a business priority at the moment. You may start tasks with one of the following labels: Priority: 1 (Normal), Priority: 2 (Medium), Priority: 3 (High), Priority: 4 (Urgent), Priority: 5 (Emergency)",
+      },
+    });
   });
 });
