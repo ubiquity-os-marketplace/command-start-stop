e1062fdd5f935e730b468de53f2dad01d36f3ced
gentlementlegen <fernand.veyrier@epitech.eu>
2025-04-29 01:52:40 +0900

    chore: remove compiled index.js from distribution
    Deleted the `dist/index.js` file to clean up compiled outputs.

--- src/functions/http-trigger.ts
+++ src/functions/http-trigger.ts
@@ -1,6 +1,32 @@
 import { app } from "@azure/functions";
 import { azureHonoHandler } from "@marplex/hono-azurefunc-adapter";
-import { app as honoApp } from "../worker";
+import { createPlugin } from "@ubiquity-os/plugin-sdk";
+import { Manifest } from "@ubiquity-os/plugin-sdk/manifest";
+import { LOG_LEVEL, LogLevel } from "@ubiquity-os/ubiquity-os-logger";
+import manifest from "../../manifest.json";
+import { createAdapters } from "../adapters";
+import { startStopTask } from "../plugin";
+import { Env, envSchema, PluginSettings, pluginSettingsSchema, SupportedEvents } from "../types";
+import { Command } from "../types/command";
+
+const honoApp = createPlugin<PluginSettings, Env, Command, SupportedEvents>(
+  (context) => {
+    return startStopTask({
+      ...context,
+      adapters: {} as ReturnType<typeof createAdapters>,
+      organizations: [],
+    });
+  },
+  manifest as Manifest,
+  {
+    envSchema: envSchema,
+    postCommentOnError: true,
+    settingsSchema: pluginSettingsSchema,
+    logLevel: (process.env.LOG_LEVEL as LogLevel) ?? LOG_LEVEL.INFO,
+    kernelPublicKey: process.env.KERNEL_PUBLIC_KEY,
+    bypassSignatureVerification: process.env.NODE_ENV === "local",
+  }
+);
 
 app.http("http-trigger", {
   methods: ["GET", "POST"],
--- src/worker.ts
+++ src/worker.ts
@@ -1,7 +1,7 @@
 import { createPlugin } from "@ubiquity-os/plugin-sdk";
 import { Manifest } from "@ubiquity-os/plugin-sdk/manifest";
 import { LOG_LEVEL, LogLevel } from "@ubiquity-os/ubiquity-os-logger";
-import { ExecutionContext, Hono } from "hono";
+import { ExecutionContext } from "hono";
 import manifest from "../manifest.json";
 import { createAdapters } from "./adapters";
 import { startStopTask } from "./plugin";
@@ -9,8 +9,6 @@ import { Command } from "./types/command";
 import { SupportedEvents } from "./types/context";
 import { Env, envSchema } from "./types/env";
 import { PluginSettings, pluginSettingsSchema } from "./types/plugin-input";
-
-export const app = new Hono();
 
 export default {
   async fetch(request: Request, env: Env, executionCtx?: ExecutionContext) {
@@ -32,7 +30,6 @@ export default {
         bypassSignatureVerification: process.env.NODE_ENV === "local",
       }
     );
-    app.route("", honoApp);
 
     return honoApp.fetch(request, env, executionCtx);
   },
