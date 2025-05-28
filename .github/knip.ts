b719b053a1c100aa561ff24263b143733160c00f
gentlementlegen <fernand.veyrier@epitech.eu>
2025-05-28 18:11:41 +0900

    chore: fixed knip

--- .github/knip.ts
+++ .github/knip.ts
@@ -1,7 +1,7 @@
 import type { KnipConfig } from "knip";
 
 const config: KnipConfig = {
-  entry: ["src/index.ts", "src/worker.ts"],
+  entry: ["src/index.ts", "src/worker.ts", "src/deno.ts"],
   project: ["src/**/*.ts"],
   ignore: ["src/types/config.ts", "**/__mocks__/**", "**/__fixtures__/**", "src/worker.ts", "dist/**"],
   ignoreExportsUsedInFile: true,
