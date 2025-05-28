d1c249df88316dba28b01d1ce0a37a7e5808c93c
gentlementlegen <fernand.veyrier@epitech.eu>
2025-05-28 17:30:14 +0900

    chore: removed unused files

--- src/deno.ts
+++ src/deno.ts
@@ -1,7 +1,7 @@
 import { createPlugin } from "@ubiquity-os/plugin-sdk";
 import { Manifest } from "@ubiquity-os/plugin-sdk/manifest";
 import { LOG_LEVEL, LogLevel } from "@ubiquity-os/ubiquity-os-logger";
-import { ExecutionContext } from "npm:hono";
+import { ExecutionContext } from "hono";
 import manifest from "../manifest.json" with { type: "json" };
 import { createAdapters } from "./adapters/index.ts";
 import { startStopTask } from "./plugin.ts";
