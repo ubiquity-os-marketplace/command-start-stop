daf24361de610b6ebd7e703237a572afa001dbf4
gentlementlegen <fernand.veyrier@epitech.eu>
2025-10-21 16:00:06 +0900

    chore: remove unused Label type from payload.ts

--- src/types/payload.ts
+++ src/types/payload.ts
@@ -1,7 +1,6 @@
 import { RestEndpointMethodTypes } from "@octokit/plugin-rest-endpoint-methods";
 
 export type Issue = RestEndpointMethodTypes["issues"]["get"]["response"]["data"];
-export type Label = RestEndpointMethodTypes["issues"]["listLabelsOnIssue"]["response"]["data"][0];
 export type Review = RestEndpointMethodTypes["pulls"]["listReviews"]["response"]["data"][0];
 export type TimelineEventResponse = RestEndpointMethodTypes["issues"]["listEventsForTimeline"]["response"];
 export type TimelineEvents = RestEndpointMethodTypes["issues"]["listEventsForTimeline"]["response"]["data"][0];
