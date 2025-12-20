fee37d080d0e7cbc969ca314301d4c63e188bedc
gentlementlegen <fernand.veyrier@epitech.eu>
2025-12-20 09:50:51 +0100

    chore: support user Octokit creation with installation token
    Updated helper to use user Octokit if JWT is an installation token.

--- src/handlers/start/api/helpers/get-plugin-config.ts
+++ src/handlers/start/api/helpers/get-plugin-config.ts
@@ -4,8 +4,10 @@ import manifest from "../../../../../manifest.json" with { type: "json" };
 import { Logs } from "../../../../types/context";
 import { Env } from "../../../../types/env";
 import { PluginSettings } from "../../../../types/plugin-input";
+import { isInstallationToken } from "../../../../utils/token";
 import { createOctokitInstances } from "./config/fetching";
 import { getDefaultConfig } from "./context-builder";
+import { createUserOctokit } from "./octokit";
 import { parseIssueUrl } from "./parsers";
 import { StartQueryParams } from "./types";
 
@@ -14,19 +16,27 @@ export async function fetchMergedPluginSettings({
   issueUrl,
   logger,
   environment,
+  jwt,
 }: {
   env: Env;
   issueUrl: string;
   logger: Logs;
   environment: StartQueryParams["environment"];
+  jwt: string;
 }): Promise<PluginSettings> {
   const repoParts = parseIssueUrl(issueUrl, logger);
   const { owner, repo } = repoParts;
 
   // Use App-authenticated Octokit to read configs (user PAT may lack perms)
-  const { orgOctokit, repoOctokit } = await createOctokitInstances(env, owner, repo, logger);
+  let octokitInstance;
+  if (isInstallationToken(jwt)) {
+    octokitInstance = await createUserOctokit(jwt);
+  } else {
+    const { orgOctokit, repoOctokit } = await createOctokitInstances(env, owner, repo, logger);
+    octokitInstance = repoOctokit ?? orgOctokit;
+  }
 
-  const cfgParser = new ConfigurationHandler(logger, repoOctokit ?? orgOctokit, environment ?? null);
+  const cfgParser = new ConfigurationHandler(logger, octokitInstance, environment ?? null);
 
   return (await cfgParser.getSelfConfiguration<PluginSettings>(manifest as Manifest, { owner, repo })) || getDefaultConfig();
 }
