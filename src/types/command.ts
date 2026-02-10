5963ac38e4fa2c52709fedbe24524dec8a53dc0a
gentlementlegen <fernand.veyrier@epitech.eu>
2026-02-10 09:17:25 +0900

    feat: enhance command schema with descriptions and examples
    Added descriptions and examples to the `start` and `stop` commands for better clarity.

--- src/types/command.ts
+++ src/types/command.ts
@@ -1,15 +1,24 @@
-import { Type as T } from "@sinclair/typebox";
-import { StaticDecode } from "@sinclair/typebox";
+import { StaticDecode, Type as T } from "@sinclair/typebox";
 
 export const startCommandSchema = T.Object({
-  name: T.Literal("start"),
+  name: T.Literal("start", {
+    examples: ["/start"],
+    description: "Assign yourself and/or others to the issue/task.",
+  }),
   parameters: T.Object({
-    teammates: T.Array(T.String()),
+    teammates: T.Optional(
+      T.Array(T.String({ description: "GitHub username" }), {
+        description: "Users other than yourself to assign to the issue",
+      })
+    ),
   }),
 });
 
 export const stopCommandSchema = T.Object({
-  name: T.Literal("stop"),
+  name: T.Literal("stop", {
+    description: "Unassign yourself from the issue/task.",
+    examples: ["/stop"],
+  }),
 });
 
 export const commandSchema = T.Union([startCommandSchema, stopCommandSchema]);
