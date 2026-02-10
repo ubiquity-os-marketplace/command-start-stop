d97fbc36877ce31453461a8973e9ba17fbd673b3
gentlementlegen <fernand.veyrier@epitech.eu>
2026-02-10 16:40:55 +0900

    fix: sync workflow skipBotEvents and parameter metadata

--- src/types/command.ts
+++ src/types/command.ts
@@ -4,7 +4,7 @@ import { StaticDecode } from "@sinclair/typebox";
 export const startCommandSchema = T.Object({
   name: T.Literal("start", { description: 'Assign yourself and/or others to the issue/task.', examples: ['/start'] }),
   parameters: T.Object({
-    teammates: T.Array(T.String()),
+    teammates: T.Array(T.String(), { description: "GitHub usernames to assign to the issue/task.", examples: [["@UbiquityOS"]] }),
   }),
 });
 
