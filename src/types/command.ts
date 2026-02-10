02ff0b52a0bb78c7c9a36665e62dca4dbb892c24
gentlementlegen <fernand.veyrier@epitech.eu>
2026-02-10 15:52:16 +0900

    fix: align manifest workflow and command schema metadata

--- src/types/command.ts
+++ src/types/command.ts
@@ -2,14 +2,14 @@ import { Type as T } from "@sinclair/typebox";
 import { StaticDecode } from "@sinclair/typebox";
 
 export const startCommandSchema = T.Object({
-  name: T.Literal("start"),
+  name: T.Literal("start", { description: 'Assign yourself and/or others to the issue/task.', examples: ['/start'] }),
   parameters: T.Object({
     teammates: T.Array(T.String()),
   }),
 });
 
 export const stopCommandSchema = T.Object({
-  name: T.Literal("stop"),
+  name: T.Literal("stop", { description: 'Unassign yourself from the issue/task.', examples: ['/stop'] }),
 });
 
 export const commandSchema = T.Union([startCommandSchema, stopCommandSchema]);
